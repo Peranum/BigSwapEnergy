f654b45f88f20a7136d595d95160e62ed9c13f6d
Peranum <peranum786@gmail.com>
2025-09-17 23:54:31 +0300

    review

--- internal/shared/logger/logger.go
+++ internal/shared/logger/logger.go
@@ -1,17 +1,11 @@
 package logger
 
 import (
-	"os"
-
 	"go.uber.org/zap"
 )
 
 func NewLogger() *zap.Logger {
 	config := zap.NewProductionConfig()
-
-	if os.Getenv("DEBUG") == "true" {
-		config = zap.NewDevelopmentConfig()
-	}
 
 	logger, err := config.Build()
 	if err != nil {
